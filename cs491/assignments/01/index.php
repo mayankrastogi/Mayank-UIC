c98c0ab8600a111965cf2b01d737bec60aff2189
Mayank K Rastogi <mayankrastogi.is@gmail.com>
2018-09-06 20:33:06 -0500

    Initial commit

---
+++ cs491/assignments/01/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-58444407-3"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-58444407-3');
+	</script>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Mayank K Rastogi">
+
+    <title>CS491 Assignment 1 | Mayank@UIC</title>
+
+    <!-- Icons -->
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="/manifest.json">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#00aba9">
+    <meta name="msapplication-TileImage" content="/mstile-144x144.png">
+    <meta name="theme-color" content="#2c3e50">
+
+</head>
+<body style="height: 100%; width: 100%; padding: 0; margin: 0;">
+	<iframe src="files/01_VRPlatforms_AdvantagesAndDisadvantages.pdf" style="position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;"></iframe>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-58444407-3"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-58444407-3');
+	</script>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description"
+          content="Mayank K Rastogi is a tech enthusiast and a passionate programmer who loves solving challenging problems and likes to keep learning awesome stuff!">
+    <meta name="author" content="Mayank K Rastogi">
+
+    <title>Mayank@UIC</title>
+
+    <!-- Icons -->
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="/manifest.json">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#00aba9">
+    <meta name="msapplication-TileImage" content="/mstile-144x144.png">
+    <meta name="theme-color" content="#2c3e50">
+
+</head>
+<body>
+	<div>
+		<h2>Welcome to Mayank's homepage @ UIC.</h2>
+		<h3>The page will be up soon. Stay tuned for updates!</h3>
+		<p>Meanwhile, you may visit the following places to know more about me:</p>
+		<ul>
+			<li><a href="https://linkedin.com/in/mayankkrastogi">LinkedIn</a></li>
+			<li><a href="http://www.mayankrastogi.com">Personal Website</a></li>
+			<li><a href="https://github.com/MayankRastogi">GitHub</a></li>
+		</ul>
+		<hr />
+		<h4><i>Looking for CS 491 (428) assignment 1? Find it <a href="cs491/assignments/01/" title="">here</a></i></h4>
+	</div>
+</body>
+</html>
